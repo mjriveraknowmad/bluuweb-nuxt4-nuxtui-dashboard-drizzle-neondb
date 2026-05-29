1c7541a0262553f1b5181c9043ff9754e60fc400
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-29 13:32:05 +0200

    feat: Logica del registro de usuario hasheando la password y comprobando que no exista (además de cumplir con el schema de zod anterior )

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -13,14 +13,33 @@ const bodySchema = z.object({
 export default eventHandler(async (event) => {
   const { email, password, name } = await readValidatedBody(
     event,
-    bodySchema.parse,
+    bodySchema.parse
   );
 
-  // TODO: buscar si el usuario ya existe en la base de datos
+  // buscar si el usuario ya existe en la base de datos
+  const [user] = await db
+    .select()
+    .from(usersTable)
+    .where(eq(usersTable.email, email));
 
-  // TODO:  hash de la contraseña
+  if (user) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "User already exists with this email",
+    });
+  }
 
-  // TODO:  insertar el nuevo usuario en la base de datos
+  // hash de la contraseña
+  const hashedPassword = await hashPassword(password);
+
+  // insertar el nuevo usuario en la base de datos
+  const newUser: InsertUser = {
+    email,
+    name,
+    password: hashedPassword,
+  };
+
+  await db.insert(usersTable).values(newUser);
 
   return {
     message: "User registered successfully",
