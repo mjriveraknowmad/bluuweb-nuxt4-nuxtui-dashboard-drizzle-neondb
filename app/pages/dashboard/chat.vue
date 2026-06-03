15b9438965293da55bcd67cf9bb0c276f2cc9894
Marcos Rivera <mjrivera@knowmadmood.com>
2026-06-03 17:36:26 +0200

    feat: pagina principal de chat. El botón "nuevo chat" solo se verá cuando estemos en un chat en concreto, para crear uno nuevo; que es la pagina principal del chat.

---
+++ app/pages/dashboard/chat.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UDashboardPanel id="chat">
+    <template #header>
+      <UDashboardNavbar title="Chat">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+        <template #right>
+          <UButton
+            label="New Chat"
+            v-if="$route.path !== '/dashboard/chat'"
+            to="/dashboard/chat"
+          />
+        </template>
+      </UDashboardNavbar>
+    </template>
+    <template #body>
+      <NuxtPage />
+    </template>
+  </UDashboardPanel>
+</template>
