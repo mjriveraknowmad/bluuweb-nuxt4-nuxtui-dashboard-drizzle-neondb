01fb09d7156e55c8aecabf862861e4f22a7275f4
Marcos Rivera <mjrivera@knowmadmood.com>
2026-06-04 13:33:57 +0200

    feat: Componente Modal de confirmación

---
+++ app/components/modal-confirm.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+  description: string;
+}
+
+defineProps<Props>();
+
+const emit = defineEmits<{
+  close: [boolean];
+}>();
+</script>
+
+<template>
+  <UModal
+    :title="title"
+    :description="description"
+    @close="$emit('close', false)"
+    :dismissible="false"
+  >
+    <template #footer>
+      <div class="flex gap-2 justify-end">
+        <UButton
+          label="Cancel"
+          color="neutral"
+          variant="ghost"
+          @click="$emit('close', false)"
+        />
+        <UButton
+          label="Delete"
+          color="error"
+          @click="$emit('close', true)"
+        />
+      </div>
+    </template>
+  </UModal>
+</template>
